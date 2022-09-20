1bcc34b3dd75c88af642baaeb13df68cc8fd3e0b
er.beeps <bipinkhatiwada10@gmail.com>
2022-09-20 07:16:39 +0545

    change language from eng to nepali

--- core/templatetags/custom_tags.py
+++ core/templatetags/custom_tags.py
@@ -16,4 +16,7 @@ def filter_category(queryset, filter):
 def filter_featured(queryset, filter):
     return queryset.filter(featured=True)
 
-
+@register.filter
+def convertToNepali(input):
+    return str(input).replace(',',',').replace('.','.').replace('0','०').replace('1','१').replace('2','२').replace('3','३').replace('4','४').replace('5','५').replace('6','६').replace('7','७').replace('8','८').replace('9','९')
+    
