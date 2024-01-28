cd738cddbbc8d8aeb4c042a455dd0eda794f7022
er.beeps <bipinkhatiwada10@gmail.com>
2024-01-28 14:21:29 +0545

    make image fixed sized

--- core/context_processors.py
+++ core/context_processors.py
@@ -8,19 +8,23 @@ def custom_processor(request):
     category = categories.objects.all()
 
     if request.user.is_authenticated:
-
-        ImageFormSet = modelformset_factory(Images,
-                                            form=ImageForm
-                                            )
         postform = AdditemForm()
-        postformset = Images.objects.none
         pknew = request.user.id
         user_form = UserForm(instance=request.user)
         profile_id = UserProfile.objects.filter(user=request.user).first()
         userformset = UserProfileForm(instance=profile_id)
+        
+        partners = Partner.objects.filter(is_active=True).order_by('display_order')
+        powered_by = PoweredBy.objects.filter(is_active=True).order_by('display_order').first()
 
         return {'postForm': postform, "noodle": pknew,
                 "noodle_form": user_form,
-                "userformset": userformset, 'categories': category}
+                "userformset": userformset, 
+                'categories': category,
+                'partners': partners,
+                'powered_by': powered_by,
+                }
     else:
-        return {'categories': category}
+        return {'categories': category,
+                'partners': partners,
+                'powered_by': powered_by,}
